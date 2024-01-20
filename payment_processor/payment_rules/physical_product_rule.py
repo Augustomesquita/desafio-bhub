790cdab4b748435d5c4e37552ff59225b8be7542
Augusto Mesquita <augustomesquitasrs@gmail.com>
2024-01-20 19:19:48 -0300

    Cria regra de pagamento produto físico

---
+++ payment_processor/payment_rules/physical_product_rule.py
@@ -0,0 +1,7 @@
+from payment_processor.payment_processor import PaymentProcessor
+
+
+class PhysicalProductRule(PaymentProcessor):
+    def process_payment(self, payment):
+        print("Gerando guia de remessa para envio...")
+        print("Guia de remessa para envio gerada com sucesso!")
